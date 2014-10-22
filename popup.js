c19214620dc832f4b79d975c39649a55fb390782
angusbayley <angusbayley@gmail.com>
2014-10-22 22:44:10 +0100

    parsing response from IMDB into array of titles

--- popup.js
+++ popup.js
@@ -4,11 +4,34 @@ document.addEventListener('DOMContentLoaded', function() {
 	document.getElementById("container").insertAdjacentHTML('beforeend', "about to load");
 	var req = new XMLHttpRequest();
 	req.open('GET', path, true);
-	req.onload = displayResponse();
+	req.onload = displayResponse;
 	req.send(null);
 })
 
 function displayResponse(e) {
-	document.getElementById("container").insertAdjacentHTML('beforeend', " loaded!");			
-  document.getElementById("container").insertAdjacentHTML('beforeend', e.target.responseXML);
+	var responseText = e.target.responseText;
+  closeSpan = "</span>";
+  rightArrow = ">";
+  closeA = "</a>";
+  top250 = [];
+  titleColumnText = 'class="titleColumn"';
+  i = 1;
+  findNextTitle(responseText);
+  // document.getElementById("container").insertAdjacentHTML('beforeend', "variable will go here");
+}
+
+function findNextTitle(remainingHTML) {
+		// Get to end of start of next .titleColumn <td>
+	remainingHTML = remainingHTML.slice(remainingHTML.indexOf(titleColumnText) + titleColumnText.length);
+		// Get to end of next </span>
+	remainingHTML = remainingHTML.slice(remainingHTML.indexOf(closeSpan) + closeSpan.length);
+		// Get next title
+	var title = remainingHTML.substring(remainingHTML.indexOf(rightArrow) + rightArrow.length, remainingHTML.indexOf(closeA));
+		// Push to array
+	console.log(i + ": " + title);
+	top250.push(title);
+		// ...aaaand recur
+	i++;
+	if (i>250) { return }
+	findNextTitle(remainingHTML);
 }
