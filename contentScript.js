595feeefc0c0421f57d81fd0d77564a5b8cd93eb
Angus Bayley <angusbayley@gmail.com>
2014-10-29 20:20:59 +0000

    fixed img display bug

--- contentScript.js
+++ contentScript.js
@@ -37,41 +37,42 @@ function findNextTitle(remainingHTML) { // this is rapid
 	i++;
 	if (i>250) { return }
 	findNextTitle(remainingHTML);
+	showImages();
 }
 
 function purge() { // this is slow (4s)
-	console.log("purge starts at: " + Date.now());
+	//console.log("purge starts at: " + Date.now());
 	var movies = document.getElementsByClassName('agMovie');
 	var length = movies.length;
 	var j = 0;
 	for (k=0; k<length; k++) {
-		console.log("purge iteration starts at: " + Date.now());
+		//console.log("purge iteration starts at: " + Date.now());
 		var thisMovie = movies[j].getElementsByTagName("img")[0].alt;
 		if (!inTop250(thisMovie)) {
 			//console.log("it's not! Removing " + thisMovie)
 			movies[j].parentNode.removeChild(movies[j]);	// 	*** this is the slow line ***
-			console.log("not in top 250 block ends at: " + Date.now());
+			//console.log("not in top 250 block ends at: " + Date.now());
 		}
 		else {
 			//console.log(thisMovie + " is in the top 250. Keeping it.");
 			j++;
 		}
 	}
-	console.log("purge for loop finishes at: " + Date.now());
+	//console.log("purge for loop finishes at: " + Date.now());
 	removeEmptyRows();
 	//console.log("removeEmptyRows finishes at: " + Date.now());
 }
 
 function inTop250(movieToCheck) { // slow but not the slowest part (accounts for ~1/4 of the time)
 	// look to replace this with a built in function such as array.contains or similar - may increase speed
-	console.log("inTop250 starts at: " + Date.now());
+	//console.log("inTop250 starts at: " + Date.now());
 	for (var m=0; m<top250.length; m++) {
 		if (movieToCheck == top250[m]) {
-			console.log("inTop250 ends at: " + Date.now());
+			//console.log("inTop250 ends at: " + Date.now());
 			return true
 		}
 	}
-	console.log("inTop250 ends at: " + Date.now());
+	//console.log("inTop250 ends at: " + Date.now());
 	return false;
 }
 
@@ -99,6 +100,16 @@ function removeEmptyRows() {	// this is rapid
 	}
 }
 
+function showImages() {
+	var images = document.getElementsByTagName('img');
+	for (j=0; j<images.length; j++) {
+		var src = images[j].getAttribute('hsrc');
+		if(src) {
+			images[j].removeAttribute('hsrc');
+			images[j].setAttribute('src', src);
+		}
+	}
+}
 
 if (document.readyState == "complete") {
 	getIMDB();
