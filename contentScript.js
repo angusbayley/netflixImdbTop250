73a4c9a3de21f61009b4d1b9fcb2068be7add424
angusbayley <angusbayley@gmail.com>
2014-11-09 10:53:40 +0000

    working with straddling deletion pattern, but messy code and ux

--- contentScript.js
+++ contentScript.js
@@ -6,12 +6,12 @@ function getIMDB() { // this is quick enough (probs can't do anything about it a
 	console.log("request starts at: " + Date.now());
 	var req = new XMLHttpRequest();
 	req.open('GET', path, true);
-	req.onload = displayResponse;
+	req.onload = parseAndPurge;
 	req.send(null);
 }
 
-function displayResponse(e) {
-	console.log("displayResponse starts at: " + Date.now());
+function parseAndPurge(e) {
+	console.log("onload at: " + Date.now());
 	var responseText = e.target.responseText;
   closeSpan = "</span>";
   rightArrow = ">";
@@ -37,18 +37,13 @@ function findNextTitle(remainingHTML, i) { // this is rapid
 	i++;
 	if (i>250) { return }
 	findNextTitle(remainingHTML, i);
-	//showImages();
 }
 
 function purge() {
 	//console.log("purge starts at: " + Date.now());
 	var rows = document.getElementsByClassName("mrow");
-	var rowSchedule = rowScheduler(rows);
-	for (var i=0; i<rowSchedule.length; i++) {
-		var row = rows[rowSchedule[i]];
-		//filterRow(row);
-	}
-	//removeEmptyRows();
+	rowScheduler(rows);
+	console.log("purge finishes at: " + Date.now());
 }
 
 function rowScheduler(rows) {
@@ -61,11 +56,77 @@ function rowScheduler(rows) {
 	// to locate the row, and filter that row.
 
 	var countRows = rows.length;
-	for (var i=0; i<countRows; i++) {
-		console.log(Date.now());
-		console.log("client top clearance: " + rows[i].getBoundingClientRect().top);
+
+	var firstRowTopDiff = rows[0].getBoundingClientRect().top;
+	if(firstRowTopDiff>0) {
+		var j = 0;
+		for (var i=0; i<countRows; i++) {
+			filterRow(rows[j]);
+			showImages(rows[j]);
+			if (!removeRowIfEmpty(rows[j])) { j++ }		// evaluates false if row removed
+		}
+	}
+	else {
+		for (var i=1; i<countRows; i++) {
+			var topDiff = rows[i].getBoundingClientRect().top;
+			if (topDiff>0) {														// i now represents the first fully visible row
+				break;
+			}
+		}
+		processRow(rows, i-1);										// first row gets processed
+		processRow(rows, i);
+		if (rows[i+1]) {processRow(rows, i+1);}
+		if (rows[i+2]) {processRow(rows, i+2);}		// all rows in view at normal zoom are now processed
+
+//now, process the next row down then next row up, then down then up until all rows done:
+		
+		window.setTimeout(function() {cleanSurroundingRows(i, 2, rows)}, 1000);
 	}
 }
+
+function cleanSurroundingRows(i, j, rows) {
+	console.log("cleaning... j = " + j);
+	if (rows[i+j]) {									// checks row exists first
+		processRow(rows, (i+j));	
+	}
+	if (rows[i-j]) {									// checks row exists first
+		console.log("i: "+i);
+		rowsAboveRemoved = 0;
+		processRow(rows, (i-j), true);
+		i -= rowsAboveRemoved;
+		//console.log("shifted i: "+i);
+	}
+	console.log("function's i, j: " + i + ", " + j);
+	if (j>rows.length) {
+		return ;
+	}
+
+	window.setTimeout(function() {
+		console.log("callback's i, j: " + i + ", " + j);
+		cleanSurroundingRows(i,j+1,rows);
+	}, 100);
+}
+
+
+function processRow(rows, index, above) {	// reason for taking these arguments: see below
+	filterRow(rows[index]);
+	showImages(rows[index]);
+	if (removeRowIfEmpty(rows[index])) {							// evaluates true if row was removed
+
+		if (above) {
+			rowsAboveRemoved++;
+			index --;
+			//console.log("rowsAboveRemoved is working");
+			if (rows[index]) {															// rows is a node list so rows[index] has a different meaning now
+				processRow(rows, index, true);	// processes the row that replaced the old one
+			}
+		}
+
+		else if (rows[index]) {															// rows is a node list so rows[index] has a different meaning now
+			processRow(rows, index, false);	// processes the row that replaced the old one
+		}
+	}
+} 
 
 function filterRow(row) {
 	var movies = row.getElementsByClassName('agMovie');
@@ -97,32 +158,21 @@ function inTop250(movieToCheck) { // slow but not the slowest part (accounts for
 	return false;
 }
 
-function removeEmptyRows() {	// this is rapid
-	var rows = document.getElementsByClassName("mrow");
-	var length = rows.length;
-	var j = 0;
-	for(k=0; k<length; k++) {
-		if(rows[j].getElementsByTagName('img').length == 0) {
-			rows[j].parentNode.removeChild(rows[j]);
-		}
-		else if(rows[j].children.length==3) { //deals with rows that are empty && "based on your interest in... <img>":
-			if(rows[j].getElementsByClassName("evidence hasvids").length != 0) {
-				rows[j].parentNode.removeChild(rows[j]);
-			}
-			else {
-				//console.log(rows[j].getElementsByTagName('a')[0].innerHTML);
-				j++;
-			}
-		}
-		else {
-			//console.log(rows[j].getElementsByTagName('a')[0].innerHTML);
-			j++;
+function removeRowIfEmpty(row) {	// this is rapid
+	if(row.getElementsByTagName('img').length == 0) {
+		row.parentNode.removeChild(row);
+		return true;
+	}
+	else if(row.children.length==3) { //deals with rows that are empty && "based on your interest in... <img>":
+		if(row.getElementsByClassName("evidence hasvids").length != 0) {
+			row.parentNode.removeChild(row);
+			return true;
 		}
 	}
 }
 
-function showImages() {
-	var images = document.getElementsByTagName('img');
+function showImages(row) {
+	var images = row.getElementsByTagName('img');
 	for (j=0; j<images.length; j++) {
 		var src = images[j].getAttribute('hsrc');
 		if(src) {
